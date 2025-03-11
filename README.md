389130bd8acd8336230330b2b8a5fa1474eaca5f
Kareem <kareem@mola.tv>
2025-03-12 00:13:49 +0700

    feat: Initial commit with HashiCorp Cloud Platform

---
+++ README.md
@@ -0,0 +1,3 @@
+# rdf-do-terraform-dpm
+
+Ini adalah repository untuk menyimpan Terraform state dari Infrastruktur ryaka.dev. Terraform state disimpan _remotely_ di HCP Terraform (referensi: https://developer.hashicorp.com/terraform/tutorials/cloud/cloud-migrate )
---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  cloud {
+    hostname = "app.terraform.io" # karena pakai OpenTofu, jadi perlu specify hostname
+    organization = "RyakaDev"
+    workspaces {
+      name = "atlantis"
+    }
+  }
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.3.2"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+
+variable "name_length" {
+  description = "The number of words in the pet name"
+  default     = "3"
+}
+
+resource "random_pet" "pet_name" {
+  length    = var.name_length
+  separator = "-"
+}
+
+output "pet_name" {
+  value = random_pet.pet_name.id
+}
